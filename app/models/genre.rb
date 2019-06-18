96dc1630aa962bd55a49b46c363c9f7dc8a8cda4
jamieberrier <berrier.jamie@gmail.com>
2019-06-18 17:46:03 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -16,7 +16,7 @@ class Genre < ActiveRecord::Base
     # return an array of strings containing every musician's name
     binding.pry
     self.artists.collect do |t|
-
+      t.name
     end
   end
 end
